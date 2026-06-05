3ee3ed6d90e13691e6000a8a10c859e751c5ff17
linhanye@hqkj.com <linhanye@hqkj.com>
2026-06-05 18:11:28 +0800

    Enrich return exchange order items

--- src/demos/return-exchange/data/orderItems.ts
+++ src/demos/return-exchange/data/orderItems.ts
@@ -2,7 +2,7 @@ import type { OrderItem } from '../types'
 
 export const orderItems: OrderItem[] = [
   {
-    id: 'jacket',
+    id: 'jacket-l-camo-line',
     name: 'Waterproof Hunting Jacket',
     details: 'Size: L, Color: Camo',
     image:
@@ -16,6 +16,39 @@ export const orderItems: OrderItem[] = [
       { id: 'jacket-xl-camo', size: 'XL', color: 'Camo', details: 'Size: XL, Color: Camo' },
       { id: 'jacket-xl-black', size: 'XL', color: 'Black', details: 'Size: XL, Color: Black' },
       { id: 'jacket-m-camo', size: 'M', color: 'Camo', details: 'Size: M, Color: Camo' },
+    ],
+  },
+  {
+    id: 'jacket-xl-black-line',
+    name: 'Waterproof Hunting Jacket',
+    details: 'Size: XL, Color: Black',
+    image:
+      'https://images.unsplash.com/photo-1481140717212-b0124736c90a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxodW50aW5nJTIwamFja2V0JTIwd2F0ZXJwcm9vZiUyMGNhbW98ZW58MXx8fHwxNzgwNDc2NjQ4fDA&ixlib=rb-4.1.0&q=80&w=1080',
+    maxQuantity: 1,
+    quantity: 1,
+    price: 129.99,
+    variants: [
+      { id: 'jacket-l-camo', size: 'L', color: 'Camo', details: 'Size: L, Color: Camo' },
+      { id: 'jacket-l-black', size: 'L', color: 'Black', details: 'Size: L, Color: Black' },
+      { id: 'jacket-xl-camo', size: 'XL', color: 'Camo', details: 'Size: XL, Color: Camo' },
+      { id: 'jacket-xl-black', size: 'XL', color: 'Black', details: 'Size: XL, Color: Black' },
+      { id: 'jacket-m-camo', size: 'M', color: 'Camo', details: 'Size: M, Color: Camo' },
+    ],
+  },
+  {
+    id: 'gloves-moss-line',
+    name: 'Cold Weather Hunting Gloves',
+    details: 'Size: M, Color: Moss',
+    image:
+      'https://images.unsplash.com/photo-1516762689617-e1cffcef479d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1080',
+    maxQuantity: 3,
+    quantity: 3,
+    price: 34.99,
+    variants: [
+      { id: 'gloves-m-moss', size: 'M', color: 'Moss', details: 'Size: M, Color: Moss' },
+      { id: 'gloves-m-black', size: 'M', color: 'Black', details: 'Size: M, Color: Black' },
+      { id: 'gloves-l-moss', size: 'L', color: 'Moss', details: 'Size: L, Color: Moss' },
+      { id: 'gloves-l-black', size: 'L', color: 'Black', details: 'Size: L, Color: Black' },
     ],
   },
   {
